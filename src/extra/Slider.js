b8b2ac04afdd42dd3ce58624873ee80a6e01333a
Fabi.Arv <32752596+fabi2017@users.noreply.github.com>
2022-07-01 22:41:30 -0300

    Update Slider.js

--- src/extra/Slider.js
+++ src/extra/Slider.js
@@ -5,22 +5,22 @@ function Slider() {
   return (
     <Carousel className="contSlider" variant="dark">
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="Gancia-logo.png" alt="Gancia slide" />
+        <img className="imgSlide" src="/Gancia-logo.png" alt="Gancia slide" />
       </Carousel.Item>
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="Miller-logo.png" alt="Miller slide" />
+        <img className="imgSlide" src="/Miller-logo.png" alt="Miller slide" />
       </Carousel.Item>
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="smirnoff-logo.png" alt="Smirnoff slide" />
+        <img className="imgSlide" src="/smirnoff-logo.png" alt="Smirnoff slide" />
       </Carousel.Item>
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="Heineken-logo.png" alt="Heineken slide" />
+        <img className="imgSlide" src="/Heineken-logo.png" alt="Heineken slide" />
       </Carousel.Item>
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="Gordons-logo.png" alt="Gordons slide" />
+        <img className="imgSlide" src="/Gordons-logo.png" alt="Gordons slide" />
       </Carousel.Item>
       <Carousel.Item className="slider" interval={1200}>
-        <img className="imgSlide" src="andes-logo.png" alt="Andes slide" />
+        <img className="imgSlide" src="/andes-logo.png" alt="Andes slide" />
       </Carousel.Item>
     </Carousel>
   );
